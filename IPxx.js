718cfaea24ff55fbf10d9ef65d4bcc68bdb55662
gdopera <42483260+gdopera@users.noreply.github.com>
2022-10-15 17:04:03 +0800

    Create IPxx.js

---
+++ IPxx.js
@@ -0,0 +1,14 @@
+$httpClient.get("https://forge.speedtest.cn/api/location/info", function (error, response, data) {
+    let dataObject = JSON.parse(data);
+    let { country, province, isp, city, ip } = dataObject;
+    isp = `运营商：${isp}`;
+    ip = `IP：${ip}`;
+    let region = `地区：${country} ${province} ${city}`;
+    body={
+        title: "当前信息",
+        content: `${isp}\n${ip}\n${region}`,
+        backgroundColor: "#FF6600",
+        icon: "antenna.radiowaves.left.and.right.circle.fill",
+    }
+    $done(body)
+})
