936251663c94a14948e9ad50e10743923cf3eed2
gdopera <42483260+gdopera@users.noreply.github.com>
2022-10-15 18:25:08 +0800

    Update IPxx.js

--- IPxx.js
+++ IPxx.js
@@ -5,7 +5,7 @@ $httpClient.get("https://forge.speedtest.cn/api/location/info", function (error,
     ip = `IP：${ip}`;
     let region = `地区：${country} ${province} ${city}`;
     body={
-        title: "当前信息",
+        title: "当前节点信息",
         content: `${isp}\n${ip}\n${region}`,
         backgroundColor: "#FF6600",
         icon: "antenna.radiowaves.left.and.right.circle.fill",
