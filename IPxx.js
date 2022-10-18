25096bb8c0caf8fe9333047f9c811528d2ce0306
gdopera <42483260+gdopera@users.noreply.github.com>
2022-10-18 13:00:33 +0800

    Update IPxx.js

--- IPxx.js
+++ IPxx.js
@@ -1,9 +1,9 @@
 $httpClient.get("https://forge.speedtest.cn/api/location/info", function (error, response, data) {
     let dataObject = JSON.parse(data);
     let { country, province, isp, city, ip } = dataObject;
-    isp = `运营商：${isp}`;
-    ip = `IP信息：${ip}`;
-    let region = `地区信息：${country} ${province} ${city}`;
+    isp = `运营商: ${isp}`;
+    ip = `IP信息: ${ip}`;
+    let region = `地区信息: ${country} ${province} ${city}`;
     body={
         title: "国内节点",
         content: `${isp}\n${ip}\n${region}`,
