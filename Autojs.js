602b12c8f3cc6c459683afee2fed9a3e2d30982d
gdopera <42483260+gdopera@users.noreply.github.com>
2022-11-15 10:53:48 +0800

    Update Autojs.js

--- Autojs.js
+++ Autojs.js
@@ -220,7 +220,7 @@ async function Login(Phone,PassWd) {//登录
     content.attach="iPhone"
 
     let headerInfos=new Object()
-    headerInfos.clientType='#13.2.3#channe16.1#iPhone 12#'
+    headerInfos.clientType='#13.2#channe16.1#iPhone 12#'
     headerInfos.code='userLoginNormal'
     headerInfos.shopId='20002'
     headerInfos.source='110003'
@@ -253,7 +253,7 @@ async function Query(Login_info) {//余量原始数据
     content.fieldData=fieldData
 
     let headerInfos=new Object()
-    headerInfos.clientType='#13.2.3#channel6.1#iPhone 12#'
+    headerInfos.clientType='#13.2#channel6.1#iPhone 12#'
     headerInfos.code='qryImportantData'
     headerInfos.shopId='20002'
     headerInfos.source='110003'
@@ -294,7 +294,7 @@ async function ProductName(Login_info) {//余量原始数据
     content.attach="test"
 
     let headerInfos=new Object()
-    headerInfos.clientType='#13.2.3#channel6.1#iPhone 12#'
+    headerInfos.clientType='#13.2#channel6.1#iPhone 12#'
     headerInfos.timestamp=Ts
     headerInfos.code='userFluxPackage'
     headerInfos.shopId='20002'
