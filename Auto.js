186147d8dc9438eeaf4efd928b542025b75f4c55
gdopera <42483260+gdopera@users.noreply.github.com>
2023-01-08 13:54:06 +0800

    Update Auto.js

--- Auto.js
+++ Auto.js
@@ -221,15 +221,15 @@ function renderTpl(tpl, data) {
       .replace('[套]', data.Title)//套餐名
       .replace('[话]', '余额'+data.Bill)//话费
       .replace('[时]', '耗时:'+data.Time)//两次查询间隔时间
-      .replace('[跳]', '跳'+data.TimeLimit)//两次查询内跳点
-      .replace('[免]', '免'+data.TimeUnlimt)//两次查询内免流
-      .replace('[今跳]', '日跳'+data.TodayLimitUse)//今日跳点
-      .replace('[今免]', '日免'+data.TodayUnlimitUse)//今日免流
+      .replace('[跳]', '当前跳'+data.TimeLimit)//两次查询内跳点
+      .replace('[免]', '当前免'+data.TimeUnlimt)//两次查询内免流
+      .replace('[今跳]', '今日跳'+data.TodayLimitUse)//今日跳点
+      .replace('[今免]', '今日免'+data.TodayUnlimitUse)//今日免流
       .replace('[通用]', '通用'+data.AllLimitUse)//单周期总共通用
       .replace('[通剩]', '通剩'+data.AllLimitLeft)//
       .replace('[通总]', '通总'+data.AllLimit)//
-      .replace('[定用]', '定用'+data.AllUnlimitUse)//单周期总共免流
-      .replace('[定剩]', '定剩'+data.AllUnlimitLeft)//
+      .replace('[定用]', '白嫖'+data.AllUnlimitUse)//单周期总共免流
+      .replace('[定剩]', '未嫖'+data.AllUnlimitLeft)//
       .replace('[定总]', '定总'+data.AllUnlimit)//
       .replace('[总用]', '总用'+data.All)//总共使用流量
       .replace('[语用]', '语用'+data.AllVoiceUsed)//语音使用
